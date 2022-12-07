02042ee9477a89d6562c1453521db1b14c2a6b27
jimm89 <jimm_89@hotmail.com>
2022-12-07 11:08:30 +0000

    day 7 alternative recursive

---
+++ Python/Day 7/AOC22_7B_recursive.py
@@ -0,0 +1,53 @@
+import sys, copy, re
+from collections import defaultdict as dd
+read = sys.stdin.read
+
+f = open("AOC22_7A_in.txt")
+inp = f.read().split('\n')
+
+ans = 0
+
+G = dd(list)
+sz = dd(int)
+path = []
+
+for line in inp:
+    x = line.split()
+    if x[0] == "$" and x[1] == "cd":
+        if x[2] == "..":
+            path.pop()
+        elif not path:
+            path.append(".")
+        else:
+            path.append(path[-1] + "/" + x[2])
+        continue
+    if x[0] == "$":
+        assert x[1] == "ls"
+        continue
+    if '0' <= x[0][0] <= '9':
+        sz[path[-1]] += int(x[0])
+    else:
+        G[path[-1]].append(path[-1] + "/" + x[1])
+
+#print(G)
+
+def dfs(u):
+    for v in G[u]:
+        dfs(v)
+        sz[u] += sz[v]
+
+dfs(".")
+
+allowed = 70000000
+req = 30000000
+used = sz["."]
+unused = allowed - used
+rem = req - unused
+
+ans = 10**10
+
+for key, val in sz.items():
+    if val >= rem:
+        ans = min(ans, val)
+
+print(ans)
