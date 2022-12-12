57a11d9589e008e3ff2f18504a676a2ff0ea6500
jimm89 <jimm_89@hotmail.com>
2022-12-12 05:14:52 +0000

    day 12

---
+++ Python/Day 12/AOC22_12B.py
@@ -0,0 +1,52 @@
+import sys, copy, re, heapq as h
+from collections import defaultdict as dd
+read = sys.stdin.read
+f = open("AOC22_12A_in.txt")
+
+inp = [list(s) for s in f.read().split('\n')]
+
+n = len(inp)
+m = len(inp[0])
+
+grid = [[ord(inp[i][j]) - ord('a') for j in range(m)] for i in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == -14:
+            curr = [i, j]
+            grid[i][j] = 0
+        if grid[i][j] == -28:
+            end = [i, j]
+            grid[i][j] = 25
+
+G = dd(list)
+
+di = [0, 0, -1, 1]
+dj = [-1, 1, 0, 0]
+
+for i in range(n):
+    for j in range(m):
+        for k in range(4):
+            if 0 <= i + di[k] < n and 0 <= j + dj[k] < m and grid[i + di[k]][j + dj[k]] <= grid[i][j] + 1:
+                G[(i, j)].append((i + di[k], j + dj[k]))
+
+dist = [[100000 for j in range(m)] for i in range(n)]
+
+q = []
+
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == 0:
+            dist[i][j] = 0
+            h.heappush(q, (0, [i, j]))
+
+
+while q:
+    d, pos = h.heappop(q)
+    i, j = pos
+    for x, y in G[(i, j)]:
+        if dist[x][y] > d + 1:
+            h.heappush(q, (d + 1, [x, y]))
+            dist[x][y] = d + 1
+
+print(dist[end[0]][end[1]])
